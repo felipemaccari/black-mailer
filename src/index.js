adb8f58a4dfb801a8d781f6396c76e8161c425cd
Felipe Maccari <felipecesarmaccari@gmail.com>
2021-04-18 14:51:22 -0300

    teste

--- src/index.js
+++ src/index.js
@@ -10,8 +10,8 @@ app.post('/mail', async (request, response) => {
 
   let transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
-    port: 587,
-    secure: false,
+    port: 465,
+    secure: true,
     auth: {
       user: process.env.MAIL_USERNAME,
       pass: process.env.MAIL_PASSWORD
